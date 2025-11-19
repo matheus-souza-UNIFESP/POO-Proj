4ee7edbca6b7b704d46e223e7a1a3c97d62fa971
iclopes <icaro.clopes@gmail.com>
2025-11-19 20:37:13 -0300

    feat: adicionados metodos na classe Schedule

--- src/Schedule.ts
+++ src/Schedule.ts
@@ -2,9 +2,28 @@ import { Subject } from './Subject'
 import { User } from './User'
 
 export class Schedule {
+    public id?: number
     constructor(
         public name: string,
         public user: User,
         public subjects: Subject[]
     ) {}
+
+    addSubject(subject: Subject){
+        if(this.checkConflict(subject))
+            console.log('Conflito de horário')
+        else
+            this.subjects.push(subject)
+    }
+
+    removeSubject(subject: Subject){
+        this.subjects = this.subjects.filter(s => s.name !== subject.name)
+    }
+
+    checkConflict(subject: Subject): boolean{
+        for(let s of this.subjects){
+            s.daysOfWeek.find()//fazer depois
+        }
+        return false
+    }
 }
