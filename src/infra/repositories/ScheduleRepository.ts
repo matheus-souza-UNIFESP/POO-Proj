9f5b337955e13406506bed5be1d7a7e9e499bb85
matheus-souza-UNIFESP <kawasaki.campos@unifesp.br>
2025-11-27 17:54:14 -0300

    chore/feat: reorganizei arquivos e comecei scheduleRepository

--- src/infra/repositories/ScheduleRepository.ts
+++ src/infra/repositories/ScheduleRepository.ts
@@ -1,3 +1,42 @@
-export class ScheduleRepository {
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient()
+
+export class ScheduleRepository {
+    async getById(id: number) {
+        return prisma.schedule.findUnique({
+            where: { id },
+            include: { subjects: true }
+        });
+    }
+
+    async create(data: { name: string }) {
+        return prisma.schedule.create({
+            data
+        })
+    }
+
+    async addSubject(scheduleID: number, subjectID: number) {
+        return prisma.schedule.update({
+            where: { id: scheduleID },
+            data: {
+                subjects: {
+                    connect: { id: subjectID }
+                }
+            },
+            include: { subjects: true }
+        })
+    }
+
+    async removeSubject(scheduleID: number, subjectID: number) {
+        return prisma.schedule.update({
+            where: { id: scheduleID },
+            data: {
+                subjects: {
+                    disconnect: { id: subjectID }
+                }
+            },
+            include: { subjects: true }
+        })
+    }
 }
