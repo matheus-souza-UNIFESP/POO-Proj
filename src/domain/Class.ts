0cc216288ebadc9e8aad62806af864334cbc9a8b
matheus-souza-UNIFESP <kawasaki.campos@unifesp.br>
2025-11-29 11:55:04 -0300

    chore: Adicionei comentários a Class.ts

--- src/domain/Class.ts
+++ src/domain/Class.ts
@@ -1,7 +1,11 @@
+/*
+    Informações da aula de tal matéria
+*/
+
 export class Class {
     constructor(
-        public day: number,
-        public time: number,
+        public day: number,     //1: Segunda, 2: Terça, 3: Quarta, 4: Quinta, 5: Sexta
+        public time: number,    //1: 8h, 2: 10h, 3: 13h30, 4: 15h30, 5: 19h, 6: 21h
         public classroom: number,
         public subjectId: number,
         public id?: number
