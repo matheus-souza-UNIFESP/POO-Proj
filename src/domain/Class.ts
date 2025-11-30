d34682a05e9992efbc8ce8bad71ada42972af17b
matheus-souza-UNIFESP <kawasaki.campos@unifesp.br>
2025-11-30 15:47:05 -0300

    feat: Finalizei SubjectService

--- src/domain/Class.ts
+++ src/domain/Class.ts
@@ -7,7 +7,7 @@ export class Class {
         public day: number,     //1: Segunda, 2: Terça, 3: Quarta, 4: Quinta, 5: Sexta
         public time: number,    //1: 8h, 2: 10h, 3: 13h30, 4: 15h30, 5: 19h, 6: 21h
         public classroom: number,
-        public subjectId: number,
+        public subjectId?: number,
         public id?: number
     ) {}
 
