94c1bca556b8483ae0aa8ed62bc8f946bff547e9
matheus-souza-UNIFESP <kawasaki.campos@unifesp.br>
2025-12-01 12:52:04 -0300

    feat: Versão final com as ferramentas de administrador implementadas

--- web/routes.ts
+++ web/routes.ts
@@ -13,9 +13,6 @@ router.get("/users/me", authMiddleware, UserController.getMe)
 router.put("/users/:id/username", authMiddleware, UserController.updateUsername)
 router.put("/users/:id/password", authMiddleware, UserController.updatePassword)
 
-//ADMIN
-router.delete("/users/:id", authMiddleware, UserController.deleteUser)
-
 // -------------------- ROTAS DE MATÉRIA -------------------- //
 router.get("/subjects", SubjectController.getAll)
 router.get("/subjects/:id", SubjectController.getById)
@@ -25,11 +22,6 @@ router.get("/subjects/day/:day", SubjectController.getByDay)
 router.get("/subjects/time/:time", SubjectController.getByTime)
 router.get("/subjects/classroom/:classroom", SubjectController.getByClassroom)
 
-//ADMIN
-router.post("/subjects", authMiddleware, SubjectController.createSubject)
-router.put("/subjects/:id", authMiddleware, SubjectController.updateSubject)
-router.delete("/subjects/:id", authMiddleware, SubjectController.deleteSubject)
-
 // -------------------- ROTAS DE GRADES -------------------- //
 router.post("/schedules", authMiddleware, ScheduleController.createSchedule)
 router.get("/schedules/:id", authMiddleware, ScheduleController.getById)
@@ -38,5 +30,14 @@ router.put("/schedules/:id/rename", authMiddleware, ScheduleController.rename)
 router.post("/schedules/:id/add-subject", authMiddleware, ScheduleController.addSubject)
 router.post("/schedules/:id/remove-subject", authMiddleware, ScheduleController.removeSubject)
 router.delete("/schedules/:id", authMiddleware, ScheduleController.deleteSchedule)
+
+//ADMIN - USUÁRIOS
+router.get("/users", authMiddleware, UserController.getAllUsers)
+router.delete("/users/:id", authMiddleware, UserController.deleteUser)
+
+//ADMIN - MATÉRIAS
+router.post("/subjects", authMiddleware, SubjectController.createSubject)
+router.put("/subjects/:id", authMiddleware, SubjectController.updateSubject)
+router.delete("/subjects/:id", authMiddleware, SubjectController.deleteSubject)
 
 export default router
